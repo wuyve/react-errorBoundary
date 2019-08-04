d60e61ed33e3dcabc79e3e1bcb828b7457ba774b
wuyve <y80608991@126.com>
2019-08-04 18:14:14 +0800

    react之错误处理：错误边界

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+const Propfile = ({user}) => <div>name:{user.name}</div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {hasError: false}
+  }
+  componentDidCatch(error, info) {
+    // 显示错误的UI
+    this.setState({hasError: true})
+    // 同时输出错误日志
+    console.log(error, info)
+  }
+  render () {
+    if (this.state.hasError) {
+      return <h1>Oop, something went wrong.</h1>
+    }
+    return this.props.children
+  }
 }
-
+class App extends React.Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+      user: {name: 'react'}
+    }
+  }
+  onClick = () => {
+    this.setState({user: null})
+  }
+  render () {
+    return (
+      <div>
+        <ErrorBoundary>
+          <Propfile user={this.state.user}></Propfile>
+        </ErrorBoundary>
+        <button onClick={this.onClick}>更新</button>
+      </div>
+    )
+  }
+}
 export default App;
